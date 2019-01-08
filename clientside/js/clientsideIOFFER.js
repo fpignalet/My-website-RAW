5fda4c1765b4a5965454114ce8b147a188437832
francois pignalet <francois.pignalet@gmail.com>
2019-01-08 20:18:40 +0100

    Abendsave
    Um sicher zu sein...

--- clientside/js/clientsideIOFFER.js
+++ clientside/js/clientsideIOFFER.js
@@ -19,65 +19,41 @@ import {
 /// @param contener is the target DOM
 /// @param param maybe anything
 export function cliside_OFFERpageload(contener, param) {
-    try {
-        const srcid = "contener";
+    const srcid = "contener";
 
-        let cliside_OFFERldr = new CLISIDE_LOADER(cliside_BASEIDENT + param["load"]);
+    let cliside_OFFERldr = new CLISIDE_LOADER(cliside_BASEIDENT + param["load"]);
 
-        cliside_OFFERldr.localgetfile(contener,
-            "./clientside/cards/cardheader.html",
-            srcid,
-            "headercard",
-            () => {
-                contener.getElementById("titlepart").appendChild(contener.createTextNode("What can I do for you? And how much it costs..."))
-            }
-        );
-        cliside_OFFERldr.localgetfile(contener,
-            "./clientside/cards/cardfooter.html",
-            "contener",
-            "footer"
-        );
-        cliside_OFFERldr.localgetfile(contener,
-            "./clientside/cards/cardabout.html",
-            srcid,
-            "aboutcard"
-        );
+    cliside_OFFERldr.localgetfile(contener,
+        "./clientside/cards/cardheader.html",
+        srcid,
+        "headercard",
+        () => {
+            contener.getElementById("titlepart").appendChild(contener.createTextNode("What can I do for you? And how much it costs..."))
+        }
+    );
+    cliside_OFFERldr.localgetfile(contener,
+        "./clientside/cards/cardfooter.html",
+        "contener",
+        "footer"
+    );
+    cliside_OFFERldr.localgetfile(contener,
+        "./clientside/cards/cardabout.html",
+        srcid,
+        "aboutcard"
+    );
 
-    }
-    catch (e) {
-        console.log(e.name)
-    }
-    finally {
-        //...
-    }
 }
 
 /// @brief leav function
 /// @param contener is the target DOM
 /// @param param maybe anything
 export function cliside_OFFERpageunload(contener, param) {
-    try {
-        //...
-    }
-    catch (e) {
-        console.log(e.name)
-    }
-    finally {
-        //...
-    }
+//        alert("notyetimplemented")
 }
 
 /// @brief scroll function
 /// @param contener is the target DOM
 /// @param param maybe anything
 export function cliside_OFFERpagescroll(contener, param) {
-    try {
-        //...
-    }
-    catch (e) {
-        console.log(e.name)
-    }
-    finally {
-        //...
-    }
+//        alert("notyetimplemented")
 }
---
+++ serverside/data/BDdatafull.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: franc
+ * Date: 24.10.2018
+ * Time: 11:40
+ */
+
+/// --------------------------------------------------------
+/// @brief desc
+//$srvside_BDconnect = "use your credentials [login:pass:bdname]";
+//$srvside_BDsrvname = "use your credentials [IP addr]";
+//$srvside_BDconnect = "pignaletnb566:01g6A264d40:pignaletnb566:";
+//$srvside_BDsrvname = "pignaletnb566.mysql.db";
+$srvside_BDconnect = "fpi:01g6A264d40:fpitest:";
+$srvside_BDsrvname = "localhost";
+
+/// --------------------------------------------------------
+$srvside_BDtables = [
+    "fpi_testtable1"
+];
+
+$srvside_BDfields = [
+    "testtable1field1",
+    "testtable1field2",
+    "testtable1field3",
+    NULL
+];
+
+$srvside_BDtestvalues = [
+    21,
+    22,
+    'toto23',
+    NULL
+];
+
+
