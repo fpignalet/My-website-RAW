bbff76409b7174c7ddfbfb8e44767fbfabe922a2
francois pignalet <francois.pignalet@gmail.com>
2019-01-16 10:49:29 +0100

    Update BDdata.php

--- serverside/data/BDdata.php
+++ serverside/data/BDdata.php
@@ -8,8 +8,8 @@
 
 /// --------------------------------------------------------
 /// @brief stupid temp connection info stowage
-$srvside_BDconnect = "fpi:01g6A264d40:fpitest:";
-$srvside_BDsrvname = "localhost";
+$srvside_BDconnect = use-your-credentials"login:password:bdname:";
+$srvside_BDsrvname = use-your-credentials"localhost";
 
 /// --------------------------------------------------------
 /// @brief
